3cb2c6ace154bc8259bea77c005b98492c3f39f3
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-08-23 16:54:02 +0600

    feat: conditional rendering concept done

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ConditionalRendering from "./components/ConditionalRendering.vue";
+</script>
+
+<template>
+  <div class="w-full max-w-screen-md mx-auto px-6 py-12">
+    <ConditionalRendering />
+  </div>
+</template>
---
+++ src/components/ConditionalRendering.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+import SectionTitle from "./SectionTitle.vue";
+
+const isVueAwesome = ref(true);
+
+const handleToggle = () => {
+  isVueAwesome.value = !isVueAwesome.value;
+};
+</script>
+
+<template>
+  <div class="w-full grid gap-6">
+    <SectionTitle title="Conditional Rendering" />
+    <div
+      class="w-full max-w-96 bg-white border border-zinc-200 rounded-lg p-6 grid gap-6"
+    >
+      <button
+        @click="handleToggle"
+        class="h-9 px-3 py-1.5 rounded-lg bg-emerald-600 text-white text-sm font-medium w-fit"
+      >
+        Toggle
+      </button>
+
+      <!-- render and style with conditionally -->
+      <h3 v-if="isVueAwesome" class="text-3xl font-medium">Vue is awesome</h3>
+      <h3
+        v-else
+        class="text-3xl font-medium"
+        :class="{ 'text-red-500': !isVueAwesome }"
+      >
+        Oh no 😪
+      </h3>
+    </div>
+  </div>
+</template>
