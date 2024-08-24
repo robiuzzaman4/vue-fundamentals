204c325b452f815f88fcfcd2f2bf8c4b4247e84b
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-08-24 09:39:51 +0600

    feat: add list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import ConditionalRendering from "./components/ConditionalRendering.vue";
+import ListRendering from "./components/ListRendering.vue";
 </script>
 
 <template>
-  <div class="w-full max-w-screen-md mx-auto px-6 py-12">
+  <div class="w-full max-w-screen-md mx-auto px-6 py-12 grid gap-6">
     <ConditionalRendering />
+    <ListRendering />
   </div>
 </template>
---
+++ src/components/ListRendering.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+import SectionTitle from "./SectionTitle.vue";
+
+const items = ref([
+  { framework: "Next.js" },
+  { framework: "Nuxt.js" },
+  { framework: "Nest.js" },
+]);
+</script>
+
+<template>
+  <div class="w-full grid gap-6">
+    <SectionTitle title="List Rendering" />
+    <div
+      class="w-full max-w-96 bg-white border border-zinc-200 rounded-lg p-6 flex flex-col gap-6"
+    >
+      <h3 class="text-sm font-medium">Frameworks:</h3>
+      <!-- render list items -->
+      <ul
+        class="list-disc text-zinc-500 pl-4"
+        v-for="(item, index) in items"
+        :key="index"
+      >
+        <li class="text-sm -my-2">
+          {{ item.framework }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
